2a6d8771a8d028a63003f6490269a1021c245ca4
AliTaee <ali.taee.72@gmail.com>
2025-06-26 15:03:57 +0200

    feat(bubble-sort): add algorithm and unit test

---
+++ src/sort/BubbleSort.ts
@@ -0,0 +1,23 @@
+// V1
+// export default function bubble_sort(arr: number[]): void {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length - 1 - i; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 let temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+// }
+
+// V2
+export default function bubble_sort(arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            }
+        }
+    }
+}
